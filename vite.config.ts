1f31ade5c5d2d7b7b4cfd00136aea4ac74e844ee
Kibreab Solomon <kbieman21@yahoo.com>
2025-12-01 20:36:45 -0700

    Updated github deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/rest-countries-api-react",
+  base:"/rest-countries-api-react/",
   plugins: [
-    react({
+    react(//{
       // babel: {
       //   plugins: [['babel-plugin-react-compiler']],
       // },
-    }),
+    //}
+    ),
   ],
 })
