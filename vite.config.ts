5afbab0894cbb0c8c3aa9cc8ad21482cd0c6d718
Kibreab Solomon <kbieman21@yahoo.com>
2025-12-01 21:09:56 -0700

    Undo all the config changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/rest-countries-api-react/",
   plugins: [
-    react(//{
-      // babel: {
-      //   plugins: [['babel-plugin-react-compiler']],
-      // },
-    //}
-    )
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
   ],
 })
